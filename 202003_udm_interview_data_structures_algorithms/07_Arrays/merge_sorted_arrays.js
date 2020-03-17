c9c0b9e69c0d293efeac5fef534e6f43b887f77c
ls404 <lubomir@stefanov.biz>
2020-03-17 19:51:58 +0200

    :)

--- 202003_udm_interview_data_structures_algorithms/07_Arrays/merge_sorted_arrays.js
+++ 202003_udm_interview_data_structures_algorithms/07_Arrays/merge_sorted_arrays.js
@@ -1,5 +1,33 @@
 function mergeSortedArrays(array1, array2) {
+    const mergedArray = [];
+    let array1Item = array1[0];
+    let array2item = array2[0];
+    let i = 1;
+    let j = 1;
+
+    //Check sum
+    if (array1.length === 0) {
+        return array2;
+    }
+    if (array2.length === 0) {
+        return array1;
+    }
+
+    while (array1Item || array2item) {
+        // Next if is safeguard for undefined value
+        if (!array2item || array1Item < array2item) {
+            mergedArray.push(array1Item);
+            array1Item = array1[i];
+            i++
+
+        } else {
+            mergedArray.push(array2item);
+            array2item = array2[j];
+            j++
+        }
+    }
+    return mergedArray;
 
 }
 
-mergeSortedArrays([0,3,4,31], [4,6,30]);
+console.log(mergeSortedArrays([0,3,4,31], [4,6,30]));
